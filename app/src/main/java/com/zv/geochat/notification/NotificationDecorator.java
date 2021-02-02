60c4db47d00001eb979c9d96e8bb9d9804d310ba
Viktor Z <zv@zv.com>
2021-02-02 09:30:48 -0500

    fix notification: add channel, increase minSdkVersion

--- app/src/main/java/com/zv/geochat/notification/NotificationDecorator.java
+++ app/src/main/java/com/zv/geochat/notification/NotificationDecorator.java
@@ -1,10 +1,12 @@
 package com.zv.geochat.notification;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.util.Log;
 
 import com.zv.geochat.ChatActivity;
@@ -13,12 +15,24 @@ import com.zv.geochat.R;
 public class NotificationDecorator {
 
     private static final String TAG = "NotificationDecorator";
+    public static final String CHANNEL_ID = "geoChatChannel_1";
     private final Context context;
     private final NotificationManager notificationMgr;
 
     public NotificationDecorator(Context context, NotificationManager notificationManager) {
         this.context = context;
         this.notificationMgr = notificationManager;
+        createChannel(notificationManager, CHANNEL_ID);
+    }
+
+    private void createChannel(NotificationManager notificationManager, String channelId){
+        NotificationChannel notificationChannel = new NotificationChannel(channelId, "GeoChat Channel",
+                NotificationManager.IMPORTANCE_LOW);
+        notificationChannel.enableLights(true);
+        notificationChannel.setLightColor(Color.GREEN);
+        notificationChannel.enableVibration(true);
+        //notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        notificationManager.createNotificationChannel(notificationChannel);
     }
 
     public void displaySimpleNotification(String title, String contentText) {
@@ -38,6 +52,7 @@ public class NotificationDecorator {
                     .setContentText(contentText)
                     .setContentIntent(contentIntent)
                     .setAutoCancel(true)
+                    .setChannelId(CHANNEL_ID)
                     .build();
 
             noti.flags |= Notification.FLAG_AUTO_CANCEL;
